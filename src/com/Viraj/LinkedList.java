c84d15630390e5f0477f8b424c71d8c7272aa512
Viraj Mehta <viraj1messi10@gmail.com>
2021-12-14 19:17:57 +0530

    second commit

--- src/com/Viraj/LinkedList.java
+++ src/com/Viraj/LinkedList.java
@@ -5,5 +5,17 @@ public class LinkedList {
     public void insert(int data){
         Node node = new Node();
         node.data = data;
+        node.next = null;
+        if(head==null){
+            head = node;
+        }
+        else{
+            Node n = head; //temporary variable to hold the data that we are trying to insert
+            while(n.next!=null){
+                n = n.next; // till not null we will iterate
+            }
+            n.next = node; // when we get null as the address we will update the value...
+        }
+
     }
 }
